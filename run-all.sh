562567e1094e8b6ef744697aaf9746d97310ac8c
Loloy D <loloyd@loloyd.com>
2018-06-11 21:29:33 +0800

    Replacement of apt-get with apt and application of non-echoing password input

--- run-all.sh
+++ run-all.sh
@@ -15,10 +15,10 @@ echo "NLI: Update the system? [y/n]"
 read OPTION_YN
 if [ "$OPTION_YN" == "Y" ] || [ "$OPTION_YN" == "y" ]; then
   echo "NLI: Updating the system..."
-  echo "NLI: + apt-get update"
-  sudo apt-get update
-  echo "NLI: + apt-get upgrade"
-  sudo apt-get upgrade
+  echo "NLI: + apt update"
+  sudo apt update
+  echo "NLI: + apt upgrade"
+  sudo apt upgrade
 fi
 
 # Append favorite bindings to .bashrc
@@ -29,17 +29,6 @@ fi
 
 # Apply custom folder tree structure addendum
 ./run-part-apply-custom-folder-tree-structure-addendum.sh
-
-# Create an easy mount utility for network shares that you normally/usually use
-
-## Create integrated path for easy mount point installation
-
-echo "NLI: Create ~/nli/mounts folder? [y/n]"
-read OPTION_YN
-if [ "$OPTION_YN" == "Y" ] || [ "$OPTION_YN" == "y" ]; then
-  echo "NLI: + mkdir ~/nli/mounts"
-  mkdir ~/nli/mounts
-fi
 
 ## Create mount points.  https://www.tldp.org/LDP/Linux-Filesystem-Hierarchy/html/mnt.html says
 ## /mnt should be used for local issue (device level) mounting.  Volatile network mounts can therefore be implemented
@@ -62,24 +51,6 @@ if [ "$OPTION_YN" == "Y" ] || [ "$OPTION_YN" == "y" ]; then
   fi
 fi
 
-## Create easy access linked mount points (deprecated)
-
-#echo "NLI: Create easy access linked mount points? [y/n]"
-#read OPTION_YN
-#if [ "$OPTION_YN" == "Y" ] || [ "$OPTION_YN" == "y" ]; then
-#  echo "NLI: Creating easy access linked mount points..."
-#  if [ "$OPTION_PROFILE" -eq 1 ]; then
-#    ln -s /mnt/video100 ~/nli/mounts/video100
-#    ln -s /mnt/photos100 ~/nli/mounts/photos100
-#    ln -s /mnt/audio100 ~/nli/mounts/audio100
-#    ln -s /mnt/restricted100 ~/nli/mounts/restricted100
-#  elif [ "$OPTION_PROFILE" -eq 2 ]; then
-#    ln -s /mnt/ldnas138-vault ~/nli/mounts/ldnas138-vault
-#    ln -s /mnt/ldnas138-media ~/nli/mounts/ldnas138-media
-#    ln -s /mnt/ldnas138-zaux ~/nli/mounts/ldnas138-zaux
-#  fi
-#fi
-
 ## Create network shares mounting script
 
 echo "NLI: Create network shares mounting script? [y/n]"
@@ -92,7 +63,7 @@ if [ "$OPTION_YN" == "Y" ] || [ "$OPTION_YN" == "y" ]; then
     echo "NLI: Input the username for mounting home-nas1:"
     read OPTION_USERNAME
     echo "NLI: Input the password of $OPTION_USERNAME for mounting home-nas1:"
-    read OPTION_PASSWORD
+    read -s OPTION_PASSWORD
     touch ~/nli/mounts/mount-home-nas1.sh
     printf "sudo mount -t cifs //$OPTION_IPADDRESS/Audio ~/nli/mounts/home-nas1-audio -o guest,rw,uid=1000,gid=1000,nounix,iocharset=utf8,file_mode=0777,dir_mode=0777\n" >> ~/nli/mounts/mount-home-nas1.sh
     printf "sudo mount -t cifs //$OPTION_IPADDRESS/Photos ~/nli/mounts/home-nas1-photos -o guest,rw,uid=1000,gid=1000,nounix,iocharset=utf8,file_mode=0777,dir_mode=0777\n" >> ~/nli/mounts/mount-home-nas1.sh
@@ -106,7 +77,7 @@ if [ "$OPTION_YN" == "Y" ] || [ "$OPTION_YN" == "y" ]; then
     echo "NLI: Input the username for mounting office-nas1:"
     read OPTION_USERNAME
     echo "NLI: Input the password of $OPTION_USERNAME for mounting office-nas1"
-    read OPTION_PASSWORD
+    read -s OPTION_PASSWORD
     touch ~/nli/mounts/mount-office-nas1.sh
     printf "sudo mount -t cifs //$OPTION_IPADDRESS/media ~/nli/mounts/office-nas1-media -o username=$OPTION_USERNAME,password=$OPTION_PASSWORD,vers=2.0\n" >> ~/nli/mounts/mount-office-nas1.sh
     printf "sudo mount -t cifs //$OPTION_IPADDRESS/revault ~/nli/mounts/office-nas1-revault -o username=$OPTION_USERNAME,password=$OPTION_PASSWORD,vers=2.0\n" >> ~/nli/mounts/mount-office-nas1.sh
