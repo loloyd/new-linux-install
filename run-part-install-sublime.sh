e582a598e127aa51a50fd35c6ed5ddcce576613e
Loloy D <loloyd@loloyd.com>
2018-06-15 11:06:04 +0800

    Addition of SideBarEnhancement plugin to Sublime installation

--- run-part-install-sublime.sh
+++ run-part-install-sublime.sh
@@ -11,5 +11,8 @@ if [ "$OPTION_YN" == "Y" ] || [ "$OPTION_YN" == "y" ]; then
   echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
   sudo apt update
   sudo apt install sublime-text
+  cd ~/.config/sublime-text-3/Packages
+  git clone https://github.com/SideBarEnhancements-org/SideBarEnhancements.git
+  cd ~
   # read -p "Press [Enter] key to continue..."
 fi
