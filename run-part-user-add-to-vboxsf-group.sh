f811fbcb600a99cf529eba61703ce38b42cb821c
loloyd <loloyd@loloyd.com>
2018-09-03 08:51:53 +0800

    Fixing of ommited $ bug in run-part-user-add-to-vboxsf...

--- run-part-user-add-to-vboxsf-group.sh
+++ run-part-user-add-to-vboxsf-group.sh
@@ -6,5 +6,5 @@ echo "NLI: ========================================"
 read -p "NLI: Add a user to vboxsf group now? [y/n] " OPTION_YN
 if [ "$OPTION_YN" == "Y" ] || [ "$OPTION_YN" == "y" ]; then
   read -p "NLI: Input the username for adding to vboxsf group: " OPTION_USERNAME
-  sudo usermod -G vboxsf -a OPTION_USERNAME
+  sudo usermod -G vboxsf -a $OPTION_USERNAME
 fi
